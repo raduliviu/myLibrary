9352d0084c120789304f80d650fbdb16ddbdbd08
Radu Baltag <baltag.radu@gmail.com>
2021-10-07 21:31:42 +0200

    get and post books to db

--- controllers.js
+++ controllers.js
@@ -1,5 +1,37 @@
+const pgp = require('pg-promise')()
+const username = process.env.DB_USER
+const password = process.env.DB_PASS
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+
+const uri = `postgres://${username}:${password}@${host}:${port}/library`
+const db = pgp(uri);
+
 async function addBook(book) {
-    return book
+    const newBook = {
+        title: book.title,
+        description: book.description,
+        author_id: book.authorId
+    }
+    const result = await db.query('INSERT INTO books(${this:name}) VALUES(${this:csv})', newBook)
+    return newBook
 }
 
-module.exports = {addBook}
+async function getBooks() {
+    const books = await db.query('SELECT ${columns:name} FROM ${table:name}', {
+        columns: ['id', 'title', 'description'],
+        table: 'books'
+    });
+    return books;
+}
+
+async function getOneBook(id) {
+    const book = await db.query('SELECT ${columns:name} FROM ${table:name} WHERE id = ${bookId}', {
+        columns: ['id', 'title', 'description'],
+        table: 'books',
+        bookId: id
+    });
+    return book;
+}
+
+module.exports = {addBook, getBooks, getOneBook}
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const {addBook} = require('./controllers')
+const {addBook, getBooks, getOneBook} = require('./controllers')
 const PORT = process.env.PORT || 5000
 
 app
     .use(express.json())
     .use(cors())
+    .get("/book", async (req, res) => {
+        const result = await getBooks()
+        res.status(201).send(result)
+    })
+    .get("/book/:id", async (req, res) => {
+        const result = await getOneBook(req.params.id)
+        res.status(201).send(result)
+    })
     .post("/book", async (req, res) => {
         const newBook = req.body
         const result = await addBook(newBook)
