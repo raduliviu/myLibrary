2cb80c2f42634d02014fb5273d36c7575ac606c8
Radu Baltag <baltag.radu@gmail.com>
2021-10-09 11:06:57 +0200

    author get and post functions

--- controllers.js
+++ controllers.js
@@ -5,22 +5,17 @@ const host = process.env.DB_HOST
 const port = process.env.DB_PORT
 
 let uri = `postgres://${username}:${password}@${host}:${port}/library`
+let db
 if (process.env.DATABASE_URL) {
-    uri = process.env.DATABASE_URL + "?ssl=true"
+    db = pgp({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    })
+} else {
+    db = pgp(uri);
 }
 
-const db = pgp(uri);
-
-async function addBook(book) {
-    const newBook = {
-        title: book.title,
-        description: book.description,
-        author_id: book.authorId
-    }
-    const result = await db.query('INSERT INTO books(${this:name}) VALUES(${this:csv})', newBook)
-    return newBook
-}
-
+// Functions for Books
 async function getBooks() {
     const books = await db.query('SELECT ${columns:name} FROM ${table:name}', {
         columns: ['id', 'title', 'description'],
@@ -38,4 +33,43 @@ async function getOneBook(id) {
     return book;
 }
 
-module.exports = {addBook, getBooks, getOneBook}
+async function addBook(book) {
+    const newBook = {
+        title: book.title,
+        description: book.description,
+        author_id: book.authorId
+    }
+    await db.query('INSERT INTO books(${this:name}) VALUES(${this:csv})', newBook)
+    return newBook
+}
+
+// Functions for Authors
+async function getAuthors() {
+    const authors = await db.query('SELECT ${columns:name} FROM ${table:name}', {
+        columns: ['id', 'name', 'birthdate'],
+        table: 'author'
+    });
+    return authors;
+}
+
+async function getOneAuthor(id) {
+    const author = await db.query('SELECT ${columns:name} FROM ${table:name} WHERE id = ${authorId}', {
+        columns: ['id', 'name', 'birthdate'],
+        table: 'author',
+        authorId: id
+    });
+    return author;
+}
+
+async function addAuthor(author) {
+    const newAuthor = {
+        id: author.id,
+        name: author.name,
+        birthdate: author.birthdate
+    }
+    await db.query('INSERT INTO author(${this:name}) VALUES(${this:csv})', newAuthor)
+    return newAuthor
+}
+
+
+module.exports = {addBook, getBooks, getOneBook, getAuthors, getOneAuthor, addAuthor}
--- index.js
+++ index.js
@@ -2,23 +2,38 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const {addBook, getBooks, getOneBook} = require('./controllers')
+const {addBook, getBooks, getOneBook, getAuthors, getOneAuthor, addAuthor} = require('./controllers')
 const PORT = process.env.PORT || 5000
 
 app
     .use(express.json())
     .use(cors())
+    // Endpoints for Books
     .get("/book", async (req, res) => {
         const result = await getBooks()
-        res.status(201).send(result)
+        res.status(200).send(result)
     })
     .get("/book/:id", async (req, res) => {
         const result = await getOneBook(req.params.id)
-        res.status(201).send(result)
+        res.status(200).send(result)
     })
     .post("/book", async (req, res) => {
         const newBook = req.body
         const result = await addBook(newBook)
+        res.status(201).send(result)
+    })
+    // Endpoints for Authors
+    .get("/author", async (req, res) => {
+        const result = await getAuthors()
+        res.status(200).send(result)
+    })
+    .get("/author/:id", async (req, res) => {
+        const result = await getOneAuthor(req.params.id)
+        res.status(200).send(result)
+    })
+    .post("/author", async (req, res) => {
+        const newAuthor = req.body
+        const result = await addAuthor(newAuthor)
         res.status(201).send(result)
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
